7b461117d0572a6604ecd0de73b4aa44deaccdca
Chris Withers <chris@withers.org>
2023-07-13 08:05:20 +0100

    use myst parser for myst and rest for rest

--- conftest.py
+++ conftest.py
@@ -1,13 +1,31 @@
 from doctest import ELLIPSIS
 
 from sybil import Sybil
-from sybil.parsers.rest import DocTestParser, PythonCodeBlockParser
+from sybil.parsers.myst import (
+    DocTestDirectiveParser as MarkdownDocTestParser,
+    PythonCodeBlockParser as MarkdownPythonCodeBlockParser
+)
+from sybil.parsers.rest import (
+    DocTestParser as ReSTDocTestParser,
+    PythonCodeBlockParser as ReSTPythonCodeBlockParser
+)
 
-
-pytest_collect_file = Sybil(
+markdown_examples = Sybil(
     parsers=[
-        DocTestParser(optionflags=ELLIPSIS),
-        PythonCodeBlockParser(),
+        MarkdownDocTestParser(optionflags=ELLIPSIS),
+        MarkdownPythonCodeBlockParser(),
     ],
-    patterns=["*.md", "*.py"],
-).pytest()
+    patterns=['*.md'],
+    fixtures=['keep_seed']
+)
+
+rest_examples = Sybil(
+    parsers=[
+        ReSTDocTestParser(optionflags=ELLIPSIS),
+        ReSTPythonCodeBlockParser(),
+    ],
+    patterns=['*.py'],
+    fixtures=['keep_seed']
+)
+
+pytest_collect_file = (markdown_examples+rest_examples).pytest()
