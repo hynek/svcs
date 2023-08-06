95155fda714df48c95ea09b7d653776be3150cf5
Hynek Schlawack <hs@ox.cx>
2023-08-06 07:49:09 +0200

    Add missing overloads to flask

--- src/svcs/flask.py
+++ src/svcs/flask.py
@@ -9,15 +9,24 @@ from typing import Any, TypeVar, overload
 
 from flask import Flask, current_app, g, has_app_context
 
-from ._core import Container, Registry, ServicePing
+from ._core import (
+    T1,
+    T2,
+    T3,
+    T4,
+    T5,
+    T6,
+    T7,
+    T8,
+    T9,
+    T10,
+    Container,
+    Registry,
+    ServicePing,
+)
 
 
 FlaskAppT = TypeVar("FlaskAppT", bound=Flask)
-T1 = TypeVar("T1")
-T2 = TypeVar("T2")
-T3 = TypeVar("T3")
-T4 = TypeVar("T4")
-T5 = TypeVar("T5")
 
 
 def init_app(app: FlaskAppT, registry: Registry | None = None) -> FlaskAppT:
@@ -33,55 +42,6 @@ def init_app(app: FlaskAppT, registry: Registry | None = None) -> FlaskAppT:
     app.teardown_appcontext(teardown)
 
     return app
-
-
-@overload
-def get(svc_type: type[T1], /) -> T1:
-    ...
-
-
-@overload
-def get(svc_type1: type[T1], svc_type2: type[T2], /) -> tuple[T1, T2]:
-    ...
-
-
-@overload
-def get(
-    svc_type1: type[T1], svc_type2: type[T2], svc_type3: type[T3], /
-) -> tuple[T1, T2, T3]:
-    ...
-
-
-@overload
-def get(
-    svc_type1: type[T1],
-    svc_type2: type[T2],
-    svc_type3: type[T3],
-    svc_type4: type[T4],
-    /,
-) -> tuple[T1, T2, T3, T4]:
-    ...
-
-
-@overload
-def get(
-    svc_type1: type[T1],
-    svc_type2: type[T2],
-    svc_type3: type[T3],
-    svc_type4: type[T4],
-    svc_type5: type[T5],
-    /,
-) -> tuple[T1, T2, T3, T4, T5]:
-    ...
-
-
-def get(*svc_types: type) -> object:
-    """
-    Same as :meth:`svcs.Container.get()`, but uses container on :obj:`flask.g`.
-    """
-    _, container = _ensure_req_data()
-
-    return container.get(*svc_types)
 
 
 def get_abstract(*svc_types: type) -> Any:
@@ -199,3 +159,127 @@ def _ensure_req_data() -> tuple[Registry, Container]:
         g.svcs_container = Container(registry)
 
     return registry, g.svcs_container
+
+
+@overload
+def get(svc_type: type[T1], /) -> T1:
+    ...
+
+
+@overload
+def get(svc_type1: type[T1], svc_type2: type[T2], /) -> tuple[T1, T2]:
+    ...
+
+
+@overload
+def get(
+    svc_type1: type[T1], svc_type2: type[T2], svc_type3: type[T3], /
+) -> tuple[T1, T2, T3]:
+    ...
+
+
+@overload
+def get(
+    svc_type1: type[T1],
+    svc_type2: type[T2],
+    svc_type3: type[T3],
+    svc_type4: type[T4],
+    /,
+) -> tuple[T1, T2, T3, T4]:
+    ...
+
+
+@overload
+def get(
+    svc_type1: type[T1],
+    svc_type2: type[T2],
+    svc_type3: type[T3],
+    svc_type4: type[T4],
+    svc_type5: type[T5],
+    /,
+) -> tuple[T1, T2, T3, T4, T5]:
+    ...
+
+
+@overload
+def get(
+    svc_type1: type[T1],
+    svc_type2: type[T2],
+    svc_type3: type[T3],
+    svc_type4: type[T4],
+    svc_type5: type[T5],
+    svc_type6: type[T6],
+    /,
+) -> tuple[T1, T2, T3, T4, T5, T6]:
+    ...
+
+
+@overload
+def get(
+    svc_type1: type[T1],
+    svc_type2: type[T2],
+    svc_type3: type[T3],
+    svc_type4: type[T4],
+    svc_type5: type[T5],
+    svc_type6: type[T6],
+    svc_type7: type[T7],
+    /,
+) -> tuple[T1, T2, T3, T4, T5, T6, T7]:
+    ...
+
+
+@overload
+def get(
+    svc_type1: type[T1],
+    svc_type2: type[T2],
+    svc_type3: type[T3],
+    svc_type4: type[T4],
+    svc_type5: type[T5],
+    svc_type6: type[T6],
+    svc_type7: type[T7],
+    svc_type8: type[T8],
+    /,
+) -> tuple[T1, T2, T3, T4, T5, T6, T7, T8]:
+    ...
+
+
+@overload
+def get(
+    svc_type1: type[T1],
+    svc_type2: type[T2],
+    svc_type3: type[T3],
+    svc_type4: type[T4],
+    svc_type5: type[T5],
+    svc_type6: type[T6],
+    svc_type7: type[T7],
+    svc_type8: type[T8],
+    svc_type9: type[T9],
+    /,
+) -> tuple[T1, T2, T3, T4, T5, T6, T7, T8, T9]:
+    ...
+
+
+@overload
+def get(
+    svc_type1: type[T1],
+    svc_type2: type[T2],
+    svc_type3: type[T3],
+    svc_type4: type[T4],
+    svc_type5: type[T5],
+    svc_type6: type[T6],
+    svc_type7: type[T7],
+    svc_type8: type[T8],
+    svc_type9: type[T9],
+    svc_type10: type[T10],
+    /,
+) -> tuple[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10]:
+    ...
+
+
+def get(*svc_types: type) -> object:
+    """
+    Same as :meth:`svcs.Container.get()`, but uses container on :obj:`flask.g`.
+    """
+    _, container = _ensure_req_data()
+
+    return container.get(*svc_types)
