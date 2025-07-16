e8b3503631a0a70832aeb8cf76b81b1a3e8e6834
Pawel Lewicki <lewicki@provider.pl>
2025-07-17 00:24:40 +0200

    Add suppress_context_exit to RegisteredService, tests

--- tests/test_container.py
+++ tests/test_container.py
@@ -180,6 +180,141 @@ class TestContainer:
 
         assert is_closed
 
+    def test_suppress_context_exit_suppress(self, container, close_me):
+        """
+        The default behaviour, even if exception is raised, the factory is cleaned up.
+        """
+
+        def factory():
+            yield 42
+            close_me.close()
+
+        container.registry.register_factory(
+            int, factory, suppress_context_exit=True
+        )
+
+        with pytest.raises(ValueError), container:  # noqa: PT012
+            assert 42 == container.get(int)
+            raise ValueError
+
+        assert close_me.is_closed
+
+    def test_suppress_context_exit_no_suppress(self, container, close_me):
+        """
+        Disabled suppress_context_exit, so the factory is not cleaned up. Exception stack is passed through.
+        """
+
+        def factory():
+            yield 42
+            close_me.close()  # pragma: no cover
+
+        container.registry.register_factory(
+            int, factory, suppress_context_exit=False
+        )
+
+        with pytest.raises(ValueError), container:  # noqa: PT012
+            assert 42 == container.get(int)
+            raise ValueError
+
+        assert not close_me.is_closed
+
+    def test_suppress_context_exit_no_suppress_handled(
+        self, container, close_me, caplog
+    ):
+        """
+        Disabled suppress_context_exit, so the factory is not cleaned up. Exception stack is passed through and handled in the factory.
+        """
+
+        capture_log = Mock()
+
+        def factory():
+            try:
+                yield 42
+            except ValueError:
+                capture_log("Exception handled in factory")
+            finally:
+                close_me.close()
+
+        container.registry.register_factory(
+            int, factory, suppress_context_exit=False
+        )
+
+        with pytest.raises(ValueError), container:  # noqa: PT012
+            assert 42 == container.get(int)
+            raise ValueError
+
+        assert close_me.is_closed
+        capture_log.assert_called_once_with("Exception handled in factory")
+
+    @pytest.mark.asyncio
+    async def test_async_suppress_context_exit_async_factory_suppress(
+        self, container, close_me, caplog
+    ):
+        """
+        The default behaviour, even if exception is raised, the factory is cleaned up. Async factory.
+        """
+
+        async def factory():
+            yield 42
+            await close_me.aclose()
+
+        container.registry.register_factory(
+            int, factory, suppress_context_exit=True
+        )
+
+        with pytest.raises(ValueError):  # noqa: PT012
+            async with container:
+                assert 42 == await container.aget(int)
+                raise ValueError
+
+        assert close_me.is_aclosed
+
+    @pytest.mark.asyncio
+    async def test_async_suppress_context_exit_async_factory_no_suppress(
+        self, container, close_me, caplog
+    ):
+        """
+        Disabled suppress_context_exit, so the factory is not cleaned up. Exception stack is passed through. Async factory.
+        """
+
+        async def factory():
+            yield 42
+            await close_me.aclose()  # pragma: no cover
+
+        container.registry.register_factory(
+            int, factory, suppress_context_exit=False
+        )
+
+        with pytest.raises(ValueError):  # noqa: PT012
+            async with container:
+                assert 42 == await container.aget(int)
+                raise ValueError
+
+        assert not close_me.is_aclosed
+
+    @pytest.mark.asyncio
+    async def test_async_suppress_context_exit_sync_factory_no_suppress(
+        self, container, close_me, caplog
+    ):
+        """
+        Disabled suppress_context_exit, so the factory is not cleaned up. Exception stack is passed through. Sync factory.
+        """
+
+        def factory():
+            yield 42
+            close_me.close()  # pragma: no cover
+
+        container.registry.register_factory(
+            int, factory, suppress_context_exit=False
+        )
+
+        with pytest.raises(ValueError):  # noqa: PT012
+            async with container:
+                assert 42 == await container.aget(int)
+                raise ValueError
+
+        assert not close_me.is_closed
+
 
 class TestServicePing:
     def test_ping(self, registry, container, close_me):
