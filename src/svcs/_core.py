b5f8ac503532a8449fcf8243d307d751af48e1b7
Hynek Schlawack <hs@ox.cx>
2025-09-10 19:08:18 +0200

    Add ty, cleanup signature-getting a bit (#141)

--- src/svcs/_core.py
+++ src/svcs/_core.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 
 import inspect
 import logging
+import sys
 import warnings
 
 from collections.abc import Awaitable, Callable, Iterator
@@ -501,23 +502,41 @@ class Registry:
         self._on_close.clear()
 
 
+if sys.version_info >= (3, 10):
+
+    def _robust_signature(factory: Callable) -> inspect.Signature | None:
+        with suppress(Exception):
+            # Provide the locals so that `eval_str` will work even if the user
+            # places the `Container` under a `if TYPE_CHECKING` block.
+            return inspect.signature(
+                factory,
+                locals={"Container": Container},  # ty: ignore[unknown-argument]
+                eval_str=True,  # ty: ignore[unknown-argument]
+            )
+
+        # Retry without `eval_str` since if the annotation is "svcs.Container"
+        # the eval will fail due to it not finding the `svcs` module
+        with suppress(Exception):
+            return inspect.signature(factory)
+
+        return None
+
+else:
+
+    def _robust_signature(factory: Callable) -> inspect.Signature | None:
+        try:
+            return inspect.signature(factory)
+        except Exception:  # noqa: BLE001
+            return None
+
+
 def _takes_container(factory: Callable) -> bool:
     """
     Return True if *factory* takes a svcs.Container as its first argument.
     """
-    try:
-        # Provide the locals so that `eval_str` will work even if the user
-        # places the `Container` under a `if TYPE_CHECKING` block.
-        sig = inspect.signature(
-            factory, locals={"Container": Container}, eval_str=True
-        )
-    except Exception:  # noqa: BLE001
-        # Retry without `eval_str` since if the annotation is "svcs.Container"
-        # the eval will fail due to it not finding the `svcs` module
-        try:
-            sig = inspect.signature(factory)
-        except Exception:  # noqa: BLE001
-            return False
+
+    if not (sig := _robust_signature(factory)):
+        return False
 
     try:
         (name, p) = next(iter(sig.parameters.items()))
